acffcf8168bf4ed49b8254a60ddee7c1e512aefc
Saketh Sunil Pai <paisaketh@gmail.com>
2026-08-11 18:00:44 +0530

    Changed bootloader config for UEFI

--- modules/hosts/thor/configuration.nix
+++ modules/hosts/thor/configuration.nix
@@ -6,9 +6,12 @@ flake.nixosModules.thorConfiguration = { config, pkgs, lib, ... }: {
       self.nixosModules.ly
     ];
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/nvme0n1";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.grub = {
+    enable = true;
+    device = "nodev";
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
 
   networking.hostName = "thor";
   networking.wireless.enable = true;
