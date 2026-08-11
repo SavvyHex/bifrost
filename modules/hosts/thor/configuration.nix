d4a2ba9e3e8e871551d75ed9b26f835533ba7f3f
Saketh Sunil Pai <paisaketh@gmail.com>
2026-08-11 22:05:51 +0530

    Reformat config files

--- modules/hosts/thor/configuration.nix
+++ modules/hosts/thor/configuration.nix
@@ -1,114 +1,114 @@
 { self, inputs, ... }: {
-flake.nixosModules.thorConfiguration = { config, pkgs, lib, ... }: {
-  imports = [ 
-      self.nixosModules.thorHardware
-      self.nixosModules.niri
-      self.nixosModules.ly
+  flake.nixosModules.thorConfiguration = { config, pkgs, lib, ... }: {
+    imports = [ 
+        self.nixosModules.thorHardware
+        self.nixosModules.niri
+        self.nixosModules.ly
+      ];
+
+    boot.loader = {
+      systemd-boot.enable = false;
+
+      grub = {
+        enable = true;
+        device = "nodev";
+        efiSupport = true;
+      };
+
+      efi = {
+        canTouchEfiVariables = true;
+      };
+    };
+
+    networking.hostName = "thor";
+    networking.wireless.enable = true;
+
+    networking.networkmanager.enable = true;
+
+    time.timeZone = "Asia/Kolkata";
+
+    i18n.defaultLocale = "en_IN";
+
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "en_IN";
+      LC_IDENTIFICATION = "en_IN";
+      LC_MEASUREMENT = "en_IN";
+      LC_MONETARY = "en_IN";
+      LC_NAME = "en_IN";
+      LC_NUMERIC = "en_IN";
+      LC_PAPER = "en_IN";
+      LC_TELEPHONE = "en_IN";
+      LC_TIME = "en_IN";
+    };
+
+    services.xserver.xkb = {
+      layout = "us";
+      variant = "";
+    };
+
+    users.users."savvyhex" = {
+      isNormalUser = true;
+      description = "Saketh Sunil Pai";
+      extraGroups = [ "networkmanager" "wheel" ];
+      packages = with pkgs; [];
+    };
+
+    nixpkgs.config.allowUnfree = true;
+
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    environment.systemPackages = with pkgs; [
+      vim 
+      wget
+      python3
+      git
+      tree
+      uv
+      easyeffects
+      yt-dlp
+      blueman
+      swaylock
+      swayidle
+      mission-center
+      networkmanagerapplet
+      awww
+      jq
+      glib
+      gtk3
+      gtk4
+      gsettings-desktop-schemas
+      coreutils
+      bash
+      vscode
+      nautilus
+
+      inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
+
+      adwaita-icon-theme
+      papirus-icon-theme
+      hicolor-icon-theme
     ];
 
-  boot.loader = {
-    systemd-boot.enable = false;
+    programs.nix-ld.enable = true;
+    programs.nix-ld.libraries = with pkgs; [
+      stdenv.cc.cc.lib
+      zlib
+    ];
 
-    grub = {
-      enable = true;
-      device = "nodev";
-      efiSupport = true;
-    };
+    fonts.packages = with pkgs; [
+      material-symbols
+      nerd-fonts.jetbrains-mono   
+    ];
 
-    efi = {
-      canTouchEfiVariables = true;
-    };
+    environment.variables.GSETTINGS_SCHEMA_DIR = "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas";
+    environment.variables.LD_LIBRARY_PATH = lib.makeLibraryPath [
+      pkgs.stdenv.cc.cc.lib
+      pkgs.zlib
+    ]; 
+
+    programs.dconf.enable = true;
+
+    services.openssh.enable = true;
+
+    system.stateVersion = "26.05";
   };
-
-  networking.hostName = "thor";
-  networking.wireless.enable = true;
-
-  networking.networkmanager.enable = true;
-
-  time.timeZone = "Asia/Kolkata";
-
-  i18n.defaultLocale = "en_IN";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_IN";
-    LC_IDENTIFICATION = "en_IN";
-    LC_MEASUREMENT = "en_IN";
-    LC_MONETARY = "en_IN";
-    LC_NAME = "en_IN";
-    LC_NUMERIC = "en_IN";
-    LC_PAPER = "en_IN";
-    LC_TELEPHONE = "en_IN";
-    LC_TIME = "en_IN";
-  };
-
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  users.users."savvyhex" = {
-    isNormalUser = true;
-    description = "Saketh Sunil Pai";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
-  };
-
-  nixpkgs.config.allowUnfree = true;
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  environment.systemPackages = with pkgs; [
-    vim 
-    wget
-    python3
-    git
-    tree
-    uv
-    easyeffects
-    yt-dlp
-    blueman
-    swaylock
-    swayidle
-    mission-center
-    networkmanagerapplet
-    awww
-    jq
-    glib
-    gtk3
-    gtk4
-    gsettings-desktop-schemas
-    coreutils
-    bash
-    vscode
-    nautilus
-
-    inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
-
-    adwaita-icon-theme
-    papirus-icon-theme
-    hicolor-icon-theme
-  ];
-
-  programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = with pkgs; [
-    stdenv.cc.cc.lib
-    zlib
-  ];
-
-  fonts.packages = with pkgs; [
-    material-symbols
-    nerd-fonts.jetbrains-mono   
-  ];
-
-   environment.variables.GSETTINGS_SCHEMA_DIR = "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas";
-   environment.variables.LD_LIBRARY_PATH = lib.makeLibraryPath [
-     pkgs.stdenv.cc.cc.lib
-     pkgs.zlib
-   ]; 
-
-   programs.dconf.enable = true;
-
-   services.openssh.enable = true;
-
-  system.stateVersion = "26.05";
-};
 }
--- modules/hosts/thor/hardware-configuration.nix
+++ modules/hosts/thor/hardware-configuration.nix
@@ -1,31 +1,30 @@
 { self, inputs, ... }: {
-flake.nixosModules.thorHardware = 
-{ config, lib, pkgs, modulesPath, ... }: {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  flake.nixosModules.thorHardware = { config, lib, pkgs, modulesPath, ... }: {
+    imports =
+      [ (modulesPath + "/installer/scan/not-detected.nix")
+      ];
 
-  boot.initrd.availableKernelModules = [ "vmd" "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+    boot.initrd.availableKernelModules = [ "vmd" "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
+    boot.initrd.kernelModules = [ ];
+    boot.kernelModules = [ "kvm-intel" ];
+    boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/2dd4f187-e511-409b-8d36-64a7f9695f9f";
-      fsType = "ext4";
-    };
+    fileSystems."/" =
+      { device = "/dev/disk/by-uuid/2dd4f187-e511-409b-8d36-64a7f9695f9f";
+        fsType = "ext4";
+      };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/F51B-7622";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
+    fileSystems."/boot" =
+      { device = "/dev/disk/by-uuid/F51B-7622";
+        fsType = "vfat";
+        options = [ "fmask=0077" "dmask=0077" ];
+      };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/21daa070-9118-464b-b0db-4e755a6ab310"; }
-    ];
+    swapDevices =
+      [ { device = "/dev/disk/by-uuid/21daa070-9118-464b-b0db-4e755a6ab310"; }
+      ];
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-};
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  };
 }
